26440f261ccd7960a6a387aa8c731ed8d8af4e9b
Sid0347 <siddharthgaikwad633@gmail.com>
2026-02-17 18:37:01 +0000

    feat: add PS1 prompt handling in custom shell * parse PS1 command * update prompt dynamically * handle invalid commands

--- scan_input.c
+++ scan_input.c
@@ -2,10 +2,27 @@
 
 void scan_input(char *prompt, char *input_string)
 {
-	while(1)
+	while (1)
 	{
 		printf("%s", prompt);
 		scanf("%[^\n]", input_string);
-		
+
+		/* Check input have 'PS1=' or not.*/
+		if (strncmp(input_string, "PS1=", 4) == 0)
+		{
+			for (int i = 4; input_string[i] != '\0'; i++)
+			{
+				if (input_string[i] == ' ' && input_string[i + 1] != ' ' && input_string[i + 1] != '\0')
+				{
+					printf("%s: command not found\n", input_string + 4);
+				}	
+			}
+			strcpy(prompt, input_string);
+			// break;
+		}
+		else
+		{
+			printf("%s: command not found\n", input_string + 4);
+		}
 	}
 }
