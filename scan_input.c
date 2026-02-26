d35b59fbc3e299a6e2e65db371300ce430682cde
Sid0347 <siddharthgaikwad633@gmail.com>
2026-02-26 10:04:37 +0000

    Enhance signal handling and refactor command execution flow in scan_input(). - Added signal handling during input wait state:   - SIGINT (Ctrl+C)   - SIGTSTP (Ctrl+Z)
    - Refactored command execution logic:
      - Integrated get_command() and check_command_type() for better command classification.
      - Separated BUILTIN and EXTERNAL command execution paths.
      - Implemented fork() for external commands with proper parent-child handling.
      - Parent now uses waitpid() with WUNTRACED flag.
      - Child process resets SIGINT and SIGTSTP to default before executing external commands.
    
    - Improved control flow and error handling for invalid commands

--- scan_input.c
+++ scan_input.c
@@ -1,10 +1,15 @@
 #include "header.h"
 
 char *exte_cmds[152];
+pid_t pid;
+int status;
 
 void scan_input(char *prompt, char *input_string)
 {
 	extract_external_commands(exte_cmds);
+	/* Signal Handling at the input waiting time.*/
+	signal(SIGINT, signal_handler);	  /* ctrl + c */
+	signal(SIGTSTP, signal_handler); /* ctrl + z */
 	while (1)
 	{
 		printf("%s", prompt);
@@ -32,12 +37,35 @@ void scan_input(char *prompt, char *input_string)
 				// printf("Invalid command.\n");
 				continue;
 			}
-			printf("here : %s\n", cmd);
-			// int type = check_command_type(cmd);
-			// if(type == BUILTIN)
-			// {
-			// 	execute_internal_commands()
-			// }
+			int type = check_command_type(cmd);
+			if(type == BUILTIN)
+			{
+				// printf("Built in cmd\n");
+				execute_internal_commands(input_string);
+			}
+			else if(type == EXTERNAL)
+			{
+				// printf("External cmd\n");
+				pid = fork();
+
+				if(pid > 0)
+				{
+					waitpid(pid, &status, WUNTRACED);
+				}
+				if (pid == 0)
+				{
+					/* Signal Handling at the command execution time.*/
+					signal(SIGINT, SIG_DFL);	  /* ctrl + c */
+					signal(SIGTSTP, SIG_DFL); /* ctrl + z */
+					execute_external_commands(input_string);
+
+				}
+			}
+			else
+			{
+				// printf("heee\n");
+				printf("No command found\n");
+			}
 		}
 	}
 }
